e7846996ffd1ac3de394e80e14fc2a522cf3c449
Matt9440 <78914427+Matt9440@users.noreply.github.com>
2022-01-30 21:25:53 +0000

    locate usable

---
+++ code/Systems/Players/Base/ISBasePlayer.Use.cs
@@ -0,0 +1,17 @@
+﻿using Sandbox;
+using System.Linq;
+
+namespace ImposterSyndrome.Systems.Players
+{
+	public partial class ISBasePlayer
+	{
+		private int UseRadius { get; set; } = 60;
+
+		public Entity LocateUsable()
+		{
+			var ents = Physics.GetEntitiesInSphere( Position, 50 ).OfType<IUse>().ToList();
+
+			return ents.FirstOrDefault() as Entity;
+		}
+	}
+}
