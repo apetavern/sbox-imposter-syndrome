b5c8d3b8e2b414dca88ab1d58c7d40b36db2b984
Matt9440 <78914427+Matt9440@users.noreply.github.com>
2022-01-30 19:28:05 +0000

    Add Task - bind completed css property to task competition status

---
+++ code/Systems/UI/Elements/Tasks/Task.cs
@@ -0,0 +1,16 @@
+﻿using ImposterSyndrome.Systems.Tasks;
+using Sandbox.UI;
+
+namespace ImposterSyndrome.Systems.UI
+{
+	public class Task : Label
+	{
+		public Task( BaseTask task )
+		{
+			StyleSheet.Load( "/Systems/UI/Elements/Tasks/Task.scss" );
+
+			SetText( task.GetTaskName() );
+			BindClass( "completed", () => task.Status == TaskStatus.Complete );
+		}
+	}
+}
