b589bfa59c730c95287851bf91b16bdab0b2b25e
Matt9440 <78914427+Matt9440@users.noreply.github.com>
2022-01-30 13:28:01 +0000

    Tasks panel

---
+++ code/Systems/UI/Elements/Tasks/TaskListPanel.cs
@@ -0,0 +1,23 @@
+﻿using Sandbox.UI;
+using Sandbox.UI.Construct;
+
+namespace ImposterSyndrome.Systems.UI
+{
+	public class TaskListPanel : Panel
+	{
+		public TaskListPanel()
+		{
+			StyleSheet.Load( "/Systems/UI/Elements/Tasks/TaskListPanel.scss" );
+
+			Add.Label( "Task 1" );
+			Add.Label( "Task 2" );
+			Add.Label( "Task 3" );
+			Add.Label( "Task 4" );
+		}
+
+		public override void Tick()
+		{
+			base.Tick();
+		}
+	}
+}
---
+++ code/Systems/UI/Elements/Tasks/TaskProgressPanel.cs
@@ -0,0 +1,22 @@
+﻿using Sandbox.UI;
+
+namespace ImposterSyndrome.Systems.UI
+{
+	[UseTemplate]
+	public class TaskProgressPanel : Panel
+	{
+		public Label ProgressLabel { get; set; }
+
+		public TaskProgressPanel()
+		{
+			StyleSheet.Load( "/Systems/UI/Elements/Tasks/TaskProgressPanel.scss" );
+		}
+
+		public override void Tick()
+		{
+			base.Tick();
+
+			ProgressLabel.Text = (0).ToString();
+		}
+	}
+}
