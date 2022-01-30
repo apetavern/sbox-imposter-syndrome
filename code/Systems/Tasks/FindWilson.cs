6e3403895a3759c334777ad2320e31ad88d3694b
Matt9440 <78914427+Matt9440@users.noreply.github.com>
2022-01-30 14:54:43 +0000

    FindWilson

---
+++ code/Systems/Tasks/FindWilson.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImposterSyndrome.Systems.Tasks
+{
+	public class FindWilson : BaseTask
+	{
+		public FindWilson()
+		{
+			TaskName = "Find Wilson";
+		}
+	}
+}
