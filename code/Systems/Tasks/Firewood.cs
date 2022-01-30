4daadd5dfecd67e3e643dc8c04d691fabf2e58df
Matt9440 <78914427+Matt9440@users.noreply.github.com>
2022-01-30 14:49:39 +0000

    Firewood task

---
+++ code/Systems/Tasks/Firewood.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImposterSyndrome.Systems.Tasks
+{
+	public class Firewood : BaseTask
+	{
+		public Firewood()
+		{
+			TaskName = "Gather firewood.";
+		}
+	}
+}
