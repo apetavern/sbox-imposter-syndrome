3fab6e57cfb661456947d7f1cf337b88d42294df
Matt9440 <78914427+Matt9440@users.noreply.github.com>
2022-01-30 01:33:10 +0000

    Add players role

---
+++ code/Systems/UI/Elements/PlayerRolePanel.cs
@@ -0,0 +1,20 @@
+﻿using ImposterSyndrome.Systems.Players;
+using Sandbox;
+using Sandbox.UI;
+using Sandbox.UI.Construct;
+
+namespace ImposterSyndrome.Systems.UI
+{
+	[UseTemplate]
+	public class PlayerRolePanel : Panel
+	{
+		public Label RoleLabel { get; set; }
+
+		public override void Tick()
+		{
+			base.Tick();
+
+			RoleLabel.Text = Local.Pawn is ISPlayer player ? (player.IsImposter ? "Imposter" : "Player") : "Spectator";
+		}
+	}
+}
