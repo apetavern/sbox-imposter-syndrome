ba37bdba750b1eb4d49581fcdcb9bf66f56adb15
Matt9440 <78914427+Matt9440@users.noreply.github.com>
2022-01-31 16:53:24 +0000

    Use a change callback for IsImposter

--- code/Systems/Players/Player/ISPlayer.cs
+++ code/Systems/Players/Player/ISPlayer.cs
@@ -8,11 +8,16 @@ namespace ImposterSyndrome.Systems.Players
 	public partial class ISPlayer : ISBasePlayer
 	{
 		[Net] public List<BaseTask> AssignedTasks { get; set; }
-		[Net, Local] public bool IsImposter { get; set; }
+		[Net, Local, Change] public bool IsImposter { get; set; }
 
 		public override void Respawn()
 		{
 			base.Respawn();
+		}
+
+		public void OnIsImposterChanged()
+		{
+			PlayerHudEntity.RebuildFromImposterStatus( IsImposter );
 		}
 	}
 }
