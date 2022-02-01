304bb08d4eae311765130ecb2ef140492b20dbdd
Matt9440 <78914427+Matt9440@users.noreply.github.com>
2022-02-01 08:31:36 +0000

    Make the player a spectator X seconds after they've been killed.

--- code/Systems/Players/Player/ISPlayer.cs
+++ code/Systems/Players/Player/ISPlayer.cs
@@ -6,18 +6,31 @@ namespace ImposterSyndrome.Systems.Players
 	public partial class ISPlayer : ISBasePlayer
 	{
 		[Net, Local, Change] public bool IsImposter { get; set; }
+		private TimeSince TimeSinceKilled { get; set; }
 
 		public override void OnKilled()
 		{
 			base.OnKilled();
 
+			TimeSinceKilled = 0;
 			// Log this as a new death.
 		}
 
 		public override void Simulate( Client cl )
 		{
 			if ( LifeState == LifeState.Dead )
+			{
+				if ( TimeSinceKilled > 3 && IsServer )
+				{
+					// Make this player a spectator.
+					var newPawn = new ISSpectator();
+					newPawn.Respawn();
+
+					cl.Pawn = newPawn;
+				}
+
 				return;
+			}
 
 			var controller = GetActiveController();
 			controller?.Simulate( cl, this, GetActiveAnimator() );
