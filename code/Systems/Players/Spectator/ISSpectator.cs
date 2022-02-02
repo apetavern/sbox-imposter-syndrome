c85a920225703936c3538e7221dd93850250e3a6
Matt9440 <78914427+Matt9440@users.noreply.github.com>
2022-02-02 12:22:13 +0000

    Don't use base.Respawn - added option to respawn at position

--- code/Systems/Players/Spectator/ISSpectator.cs
+++ code/Systems/Players/Spectator/ISSpectator.cs
@@ -5,17 +5,45 @@ namespace ImposterSyndrome.Systems.Players
 {
 	public partial class ISSpectator : ISBasePlayer
 	{
-		public override void Respawn()
+		public void Respawn( Vector3 position = default )
 		{
-			base.Respawn();
+			if ( position == default )
+				Game.Current?.MoveToSpawnpoint( this );
+			else
+				Position = position;
 
+			ResetInterpolation();
+
+			SetModel( "models/playermodel/terrysus.vmdl" );
+			backpack = new AnimEntity();
+			backpack.SetModel( "models/backpacks/business/susbusinessbackpack.vmdl" );
+			backpack.SetParent( this, true );
+
+			// TODO: A noclip walk controller?
+			Controller = new ISController();
+			Animator = new ISAnimator();
+			Camera = new ISCamera();
+
+			LifeState = LifeState.Alive;
+			Health = 100;
+			Velocity = Vector3.Zero;
+			WaterLevel.Clear();
+
+			EnableAllCollisions = false;
+			EnableDrawing = true;
+			EnableHideInFirstPerson = true;
+			EnableShadowInFirstPerson = true;
+
+			UpdateRenderAlpha();
+		}
+
+		private void UpdateRenderAlpha()
+		{
 			RenderColor = Color.White.WithAlpha( 0.4f );
 
 			// Update children render alpha.
 			foreach ( var child in Children.Cast<ModelEntity>() )
 				child.RenderColor = Color.White.WithAlpha( 0.4f );
-
-			EnableAllCollisions = false;
 		}
 	}
 }
