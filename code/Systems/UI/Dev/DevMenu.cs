ee97c6a6e6030fea1fc2e8fc6b188a5b07375783
Matt9440 <matt@ninetyfour.dev>
2022-02-02 22:51:30 +0000

    Cleanup

--- code/Systems/UI/Dev/DevMenu.cs
+++ code/Systems/UI/Dev/DevMenu.cs
@@ -12,41 +12,16 @@ namespace ImposterSyndrome.Systems.UI
 			BindClass( "visible", () => Input.Down( InputButton.Flashlight ) );
 
 			Add.Label( "General", "title" );
-			var buttons = Add.Panel( "buttons" );
-
-			buttons.Add.ButtonWithIcon( "Add a bot", "smart_toy", "button", () =>
-			{
-				ConsoleSystem.Run( "bot_add 0 0" );
-			} );
-
-			buttons.Add.ButtonWithIcon( "Suicide", "clear", "button", () =>
-			{
-				ConsoleSystem.Run( "kill" );
-			} );
-
-			buttons.Add.ButtonWithIcon( "Reload HUD", "refresh", "button", () =>
-			{
-				ConsoleSystem.Run( "rebuild" );
-			} );
+			var genButtons = Add.Panel( "buttons" );
+			genButtons.Add.ButtonWithIcon( "Add a bot", "smart_toy", "button", () => ConsoleSystem.Run( "bot_add 0 0" ) );
+			genButtons.Add.ButtonWithIcon( "Suicide", "clear", "button", () => ConsoleSystem.Run( "kill" ) );
+			genButtons.Add.ButtonWithIcon( "Reload HUD", "refresh", "button", () => ConsoleSystem.Run( "rebuild" ) );
 
 			Add.Label( "States", "title" );
-
-			var buttons1 = Add.Panel( "buttons" );
-
-			buttons1.Add.ButtonWithIcon( "Waiting", "schedule", "button", () =>
-			{
-				ConsoleSystem.Run( "updatestate waiting" );
-			} );
-
-			buttons1.Add.ButtonWithIcon( "Playing", "videogame_asset", "button", () =>
-			{
-				ConsoleSystem.Run( "updatestate playing" );
-			} );
-
-			buttons1.Add.ButtonWithIcon( "End Game", "close", "button", () =>
-			{
-				ConsoleSystem.Run( "updatestate end" );
-			} );
+			var stateButtons = Add.Panel( "buttons" );
+			stateButtons.Add.ButtonWithIcon( "Waiting", "schedule", "button", () => ConsoleSystem.Run( "updatestate waiting" ) );
+			stateButtons.Add.ButtonWithIcon( "Playing", "videogame_asset", "button", () => ConsoleSystem.Run( "updatestate playing" ) );
+			stateButtons.Add.ButtonWithIcon( "End Game", "close", "button", () => ConsoleSystem.Run( "updatestate end" ) );
 		}
 	}
 }
