a5334f3882984a5b6e27de1eed948c6db3a436df
Matt9440 <78914427+Matt9440@users.noreply.github.com>
2022-02-03 20:04:40 +0000

    Added PostEffects

---
+++ code/Systems/PostProcessing/PostEffects.cs
@@ -0,0 +1,49 @@
+﻿using Sandbox;
+
+namespace ImposterSyndrome.Systems.PostProcessing
+{
+	public static class PostEffects
+	{
+		public static StandardPostProcess Instance { get; set; }
+
+		public static void Setup()
+		{
+			Instance = new StandardPostProcess();
+
+			PostProcess.Add( Instance );
+			AddDefaultStack();
+		}
+
+		public static void Reset()
+		{
+			PostProcess.Clear();
+			AddDefaultStack();
+		}
+
+		static void AddDefaultStack()
+		{
+			// Vignette
+			Instance.Vignette.Intensity = 0.85f;
+			Instance.Vignette.Color = Color.Black;
+			Instance.Vignette.Enabled = true;
+
+			// Contrast
+			Instance.Contrast.Contrast = 1.02f;
+			Instance.Contrast.Enabled = true;
+
+			// Depth of field
+			Instance.DepthOfField.Radius = 13.12f;
+			Instance.DepthOfField.FStop = 0.333f;
+			Instance.DepthOfField.FocalLength = 82;
+			Instance.DepthOfField.FocalPoint = 700;
+			Instance.DepthOfField.Enabled = true;
+		}
+
+		public static void DoDeath()
+		{
+			// Contast
+			Instance.Contrast.Contrast = 0.95f;
+			Instance.Contrast.Enabled = true;
+		}
+	}
+}
