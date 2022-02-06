b5d22db467959e313a168979b2c4cb8ea8fc2053
Matt9440 <78914427+Matt9440@users.noreply.github.com>
2022-02-06 19:11:51 +0000

    Rename folder

--- code/Systems/Tasks/Drink/DrinkTask.cs
+++ code/Systems/Tasks/Drink/DrinkTask.cs
@@ -1,8 +1,4 @@
-﻿using Sandbox;
-using System.Collections.Generic;
-using System.Linq;
-
-namespace ImposterSyndrome.Systems.Tasks
+﻿namespace ImposterSyndrome.Systems.Tasks
 {
 	public partial class DrinkTask : MultipleTask
 	{
