14932823584181db9239364faeecb7b2d4117c23
Matt9440 <78914427+Matt9440@users.noreply.github.com>
2022-02-06 23:43:11 +0000

    Override reset to also delete all held wood

--- code/Systems/Entities/Tasks/FirewoodTask/WoodStockpileEntity.cs
+++ code/Systems/Entities/Tasks/FirewoodTask/WoodStockpileEntity.cs
@@ -2,6 +2,7 @@
 using ImposterSyndrome.Systems.Tasks;
 using Sandbox;
 using System;
+using System.Collections.Generic;
 
 namespace ImposterSyndrome.Systems.Entities
 {
@@ -13,6 +14,7 @@ namespace ImposterSyndrome.Systems.Entities
 		protected override string ModelPath => "models/citizen_props/newspaper01.vmdl";
 		protected override Type TargetTaskType { get; set; } = typeof( FirewoodTask );
 		protected override Type TargetSubTaskType { get; set; } = typeof( PlaceOnWoodStockpile );
+		private List<ModelEntity> WoodEntities { get; set; } = new();
 
 		public override bool OnUse( ISPlayer user, UseType useType )
 		{
@@ -21,10 +23,20 @@ namespace ImposterSyndrome.Systems.Entities
 
 			var woodModel = new ModelEntity( "models/sbox_props/low_wood_fence/low_wood_fence_beam_1_gib2.vmdl", this );
 			woodModel.Position = Position;
+			WoodEntities.Add( woodModel );
 
 			GetTaskInstance( user )?.MarkAsCompleted();
 
 			return false;
+		}
+
+		public override void Reset()
+		{
+			// Remove all wood
+			foreach ( var wood in WoodEntities )
+				wood.Delete();
+
+			base.Reset();
 		}
 	}
 }
