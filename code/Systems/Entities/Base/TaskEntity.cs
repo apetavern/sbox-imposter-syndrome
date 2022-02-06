e627da7e5ed24a9162ba3e594d5b56e1888f5d1f
Matt9440 <78914427+Matt9440@users.noreply.github.com>
2022-02-06 18:11:20 +0000

    TaskEntity better base implementation

--- code/Systems/Entities/Base/TaskEntity.cs
+++ code/Systems/Entities/Base/TaskEntity.cs
@@ -10,14 +10,6 @@ namespace ImposterSyndrome.Systems.Entities
 	{
 		protected virtual Type TargetTaskType { get; set; }
 
-		public override void Spawn()
-		{
-			base.Spawn();
-
-			SetModel( ModelPath );
-			SetupPhysicsFromModel( PhysicsMotionType.Static );
-		}
-
 		protected virtual BaseTask GetTaskInstance( ISPlayer user )
 		{
 			return user.AssignedTasks.FirstOrDefault( task => task.GetType() == TargetTaskType && task.Status == TaskStatus.Incomplete );
