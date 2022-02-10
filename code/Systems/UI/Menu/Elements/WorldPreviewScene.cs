35bf98c9f93a50543af76c72e035b8a41d86bfca
Matt9440 <78914427+Matt9440@users.noreply.github.com>
2022-02-10 17:17:09 +0000

    Utilize the SceneAnimBounds better

--- code/Systems/UI/Menu/Elements/WorldPreviewScene.cs
+++ code/Systems/UI/Menu/Elements/WorldPreviewScene.cs
@@ -9,15 +9,15 @@ namespace ImposterSyndrome.Systems.UI
 	public class WorldPreviewScene : Panel
 	{
 		private ScenePanel RenderScene { get; set; }
+		private Vector3 RenderScenePos => new Vector3( 0, 0, 20 );
 		private Angles RenderSceneAngles { get; set; } = new( 0f, 0.0f, 0.0f );
-		private Vector3 PlayerPosition => new Vector3( 40, 70, 0 );
-		private Vector3 RenderScenePos => new Vector3( 0, 0, 25 );
+		private Vector3 PlayerPosition => new Vector3( 80, -20, 0 );
 		private BBox SceneAnimBounds { get; set; }
 		private List<SceneAnimChild> SceneAnimChildren { get; set; } = new();
 
 		public WorldPreviewScene()
 		{
-			SceneAnimBounds = new BBox( PlayerPosition + Vector3.One * 180, PlayerPosition + -Vector3.One * 20 );
+			SceneAnimBounds = new BBox( PlayerPosition + Vector3.One * 120, PlayerPosition - Vector3.One * 120 );
 
 			Build();
 		}
@@ -28,15 +28,15 @@ namespace ImposterSyndrome.Systems.UI
 				new SceneAnimChild( Model.Load( "models/playermodel/terrysus.vmdl" ),
 					Transform.Zero.WithScale( 1.2f )
 					.WithPosition( PlayerPosition )
-					.WithRotation( Rotation.From( 0, -130, 0 ) ) )
+					.WithRotation( Rotation.From( 0, -200, 0 ) ) )
 			);
 
 			SceneAnimChildren.Add(
-				new SceneAnimChild( Model.Load( "models/playermodel/terrysus.vmdl" ),
-					Transform.Zero.WithScale( 1f )
-					.WithPosition( new Vector3( -120, 120, -4 ) ) )
-					.EnableWanderWithin( SceneAnimBounds )
-				);
+			new SceneAnimChild( Model.Load( "models/playermodel/terrysus.vmdl" ),
+				Transform.Zero.WithScale( 1f )
+				.WithPosition( new Vector3( -120, 120, -4 ) ) )
+				.EnableWanderWithin( SceneAnimBounds )
+			);
 		}
 
 		public override void Tick()
@@ -77,9 +77,6 @@ namespace ImposterSyndrome.Systems.UI
 
 				RenderScene.Style.Width = Length.Percent( 100 );
 				RenderScene.Style.Height = Length.Percent( 100 );
-
-				RenderScene.CameraRotation = Rotation.From( 0, 75, 0 );
-				RenderSceneAngles = RenderScene.CameraRotation.Angles();
 
 				RenderScene.AmbientColor = new Color( .25f, .15f, .15f ) * 0.5f;
 			}
