48745c043f3a69445fa1d7ba355114a9fe42c5ef
Matt9440 <78914427+Matt9440@users.noreply.github.com>
2022-02-13 13:22:48 +0000

    Remove comments, we now only take the number of tasks assigned in the menu

--- code/Systems/States/SetupState.cs
+++ code/Systems/States/SetupState.cs
@@ -40,7 +40,6 @@ namespace ImposterSyndrome.Systems.States
 		{
 			foreach ( var player in ImposterSyndrome.Instance.Players )
 			{
-				// Temporarily add all tasks. We can add a random selection of tasks later instead.
 				foreach ( var task in Library.GetAll<BaseTask>().Where( x => !x.IsAbstract && x.BaseType != typeof( SubTask ) ).OrderBy( _ => Guid.NewGuid() ).Take( GameConfig.Instance.NumberOfTasks ) )
 				{
 					var taskInstance = Library.Create<BaseTask>( task ).FlagAsFake( player.IsImposter );
