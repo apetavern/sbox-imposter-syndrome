4dd08d8e368ec4b8b85aa6b98cb8d33d898f84a0
Matt9440 <78914427+Matt9440@users.noreply.github.com>
2022-02-14 08:28:53 +0000

    Display state information

--- code/Systems/UI/Menu/MenuPanel.cs
+++ code/Systems/UI/Menu/MenuPanel.cs
@@ -1,11 +1,14 @@
 ﻿using ImposterSyndrome.Systems.States;
+using Sandbox;
 using Sandbox.UI;
+using Sandbox.UI.Construct;
 
 namespace ImposterSyndrome.Systems.UI.Menu
 {
 	public class MenuPanel : Panel
 	{
 		public static MenuPanel Instance { get; set; }
+		private Label Timer { get; set; }
 
 		public MenuPanel()
 		{
@@ -16,7 +19,28 @@ namespace ImposterSyndrome.Systems.UI.Menu
 			AddChild<MenuLeftPanel>();
 			AddChild<MenuRightPanel>();
 
+			Timer = Add.Label( "", "timer" );
+
 			BindClass( "visible", () => ImposterSyndrome.Instance.CurrentState is WaitingState );
+		}
+
+		public override void Tick()
+		{
+			base.Tick();
+
+			if ( ImposterSyndrome.Instance?.CurrentState is not WaitingState waitingState )
+				return;
+
+			if ( !waitingState.HasPrematchStarted )
+			{
+				if ( Client.All.Count < GameConfig.MinimumPlayers )
+					Timer.Text = "WAITING FOR MORE PLAYERS";
+				else
+					Timer.Text = "WAITING FOR HOST TO START";
+				return;
+			}
+
+			Timer.Text = $"STARTING IN {MathX.Clamp( (int)(waitingState.StateEndTime - Time.Now), 0, 500 )} SECONDS";
 		}
 	}
 }
