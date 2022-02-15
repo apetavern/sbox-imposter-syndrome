e7d7476b79959d65df74e61c9635fadfe37fb7f7
Matt9440 <78914427+Matt9440@users.noreply.github.com>
2022-02-15 13:05:37 +0000

    FishEntity & FishShoalEntity initial commit

---
+++ code/Systems/Entities/Tasks/FishingTask/FishEntity.cs
@@ -0,0 +1,35 @@
+﻿using Sandbox;
+
+namespace ImposterSyndrome.Systems.Entities
+{
+	public partial class FishEntity : AnimEntity
+	{
+		private FishShoalEntity ParentShoal { get; set; }
+		public bool IsHooked { get; set; }
+
+		public FishEntity AddToShoal( FishShoalEntity shoal )
+		{
+			SetModel( "models/citizen_props/sodacan01.vmdl" );
+			ParentShoal = shoal;
+
+			Position = shoal.Position + Vector3.Random * 10;
+			return this;
+		}
+
+		[Event.Tick.Server]
+		public void Tick()
+		{
+			if ( ParentShoal is null || !ParentShoal.IsValid )
+				Delete();
+
+			// Move
+			// Rotate
+			// Lookout for bait in the water, rotate towards and nibble if found
+		}
+
+		private void HookTo()
+		{
+
+		}
+	}
+}
---
+++ code/Systems/Entities/Tasks/FishingTask/FishShoalEntity.cs
@@ -0,0 +1,75 @@
+﻿using ImposterSyndrome.Systems.Players;
+using System.Linq;
+using Sandbox;
+using System.Collections.Generic;
+
+namespace ImposterSyndrome.Systems.Entities
+{
+	[Library( "is_tasks_fishshoal" )]
+	[Hammer.EntityTool( "Fish Shoal", "ImposterSyndrome", "A group of fish to fish in." )]
+	[Hammer.EditorModel( "models/editor/arrow.vmdl" )]
+	public partial class FishShoalEntity : TaskEntity
+	{
+		[Net] public override string UseName => "Fish";
+		public override bool HideWorldModel => true;
+		private int NumberOfFishInShoal { get; set; } = 5;
+		private List<FishEntity> Fish { get; set; } = new();
+		private AnimEntity Bait { get; set; }
+
+		public override void Spawn()
+		{
+			base.Spawn();
+
+			for ( int i = 0; i < NumberOfFishInShoal; i++ )
+			{
+				FishEntity fish = new FishEntity().AddToShoal( this );
+				Fish.Add( fish );
+			}
+		}
+
+		public override bool IsUsable( ISPlayer user, UseType useType )
+		{
+			if ( useType != UseType.Use )
+				return false;
+
+			return true;
+		}
+
+		public override bool OnUse( ISPlayer user, UseType useType )
+		{
+			// Cast
+			if ( Bait is null || !Bait.IsValid )
+			{
+				Bait = new AnimEntity( "models/citizen_props/hotdog01.vmdl" )
+				{
+					Position = user.Position + user.Rotation.Forward * 80
+				};
+
+				Log.Info( "Casting" );
+
+				UseName = "Reel";
+				return false;
+			}
+
+			// Reel
+			if ( Fish.Any( x => x.IsHooked ) )
+			{
+				// Caught
+				Log.Info( "Reeling | Caught a fish" );
+				GetTaskInstance( user )?.MarkAsCompleted();
+			}
+			else
+			{
+				// Didn't catch
+				Log.Info( "Reeling | Didn't catch anything" );
+			}
+
+			// Reset
+			UseName = "Cast";
+			Bait?.Delete();
+			Bait = null;
+
+			return false;
+		}
+	}
+}
