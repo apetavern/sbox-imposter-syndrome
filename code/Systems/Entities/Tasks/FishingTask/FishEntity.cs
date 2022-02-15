eedda55c3e35f18ef39e9ad4fcb2a24d0d858921
Matt9440 <78914427+Matt9440@users.noreply.github.com>
2022-02-15 13:36:07 +0000

    Make fish move within the shoal

--- code/Systems/Entities/Tasks/FishingTask/FishEntity.cs
+++ code/Systems/Entities/Tasks/FishingTask/FishEntity.cs
@@ -6,11 +6,18 @@ namespace ImposterSyndrome.Systems.Entities
 	{
 		private FishShoalEntity ParentShoal { get; set; }
 		public bool IsHooked { get; set; }
+		private Vector3 TargetPosition { get; set; }
+		private float DefaultSwimSpeed { get; set; } = 12f;
+		private float SwimSpeed { get; set; }
+		private TimeUntil TimeUntilMovementPermitted { get; set; }
 
 		public FishEntity AddToShoal( FishShoalEntity shoal )
 		{
 			SetModel( "models/citizen_props/sodacan01.vmdl" );
 			ParentShoal = shoal;
+
+			TargetPosition = PickRandomPosition();
+			SwimSpeed = DefaultSwimSpeed;
 
 			Position = shoal.Position + Vector3.Random * 10;
 			return this;
@@ -22,9 +29,34 @@ namespace ImposterSyndrome.Systems.Entities
 			if ( ParentShoal is null || !ParentShoal.IsValid )
 				Delete();
 
-			// Move
-			// Rotate
-			// Lookout for bait in the water, rotate towards and nibble if found
+			var moveDir = (TargetPosition - Position).Normal;
+			var distance = Vector3.DistanceBetween( TargetPosition, Position );
+
+			if ( distance > 4 )
+			{
+				if ( TimeUntilMovementPermitted > 0 )
+					return;
+
+				Position += moveDir * Time.Delta * SwimSpeed;
+				Rotation = Rotation.LookAt( moveDir, Vector3.Up );
+
+				SwimSpeed = (SwimSpeed - (SwimSpeed / 500)).Clamp( 0.5f, DefaultSwimSpeed );
+
+				return;
+			}
+			else
+			{
+				TargetPosition = PickRandomPosition();
+				TimeUntilMovementPermitted = Rand.Float( 0.5f, 2.5f );
+				SwimSpeed = DefaultSwimSpeed;
+			}
+
+			// TODO: Lookout for bait in the water, rotate towards and nibble if found
+		}
+
+		private Vector3 PickRandomPosition()
+		{
+			return (ParentShoal.Position + Vector3.Random + Vector3.Random * 50).WithZ( ParentShoal.Position.z + Rand.Int( -5, 5 ) );
 		}
 
 		private void HookTo()
